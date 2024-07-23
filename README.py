12d5191ea0b0d20f025fd6ad3a6dc94dcc80a042
ARTEM211222 <artemsadivskij@gmail.com>
2024-07-23 11:24:11 +0300

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,24 @@
+# sigma1
+from PyQt5.Qtcore import Qt
+from PyQt5.Widgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+W = W
+W.QApplication(400,200)
+W.QLabel('Ні, в 2015 Ви виграли фірмовий плакат ', 50,20)
+Button = Button
+Button.QPushButton('Ok', 10,10)
+W2 = W2
+
+W2.QApplication(800,400)
+W2.QLabel('в якому році канал отримас золоту кнопку Від YouTube?', 20,60)
+Button2 = Button2
+Button3 = Button3
+Button4 = Button4
+Button5 = Button5
+Button2.QPushButton(150,200)
+Button3.QPushButton(700,200)
+Button4.QPushButton(150,350)
+Button5.QPushButton(700,350)
+W2.QLebel('2005', 160,200)
+W2.QLebel('2010', 710,200)
+W2.QLebel('2015', 160,350)
+W2.QLebel('2015', 710,350)
